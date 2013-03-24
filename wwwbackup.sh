4fc7e7f4c8631ce5030a311b02dc7456c5b7d5f1
hawkway <highway803@gmail.com>
2013-03-24 02:53:20 -0400

    updated Scripts to current working copy

--- wwwbackup.sh
+++ wwwbackup.sh
@@ -4,6 +4,6 @@ DATE=`date '+%Y-%m-%d-%H%M%S'`
 
 cd ~
 
-tar -zcvf ~/Dropbox/web_archive/template_bak_${DATE}.tar.gz Downloads
+tar -zcvf ~/Dropbox/www_archive/template_bak_${DATE}.tar.gz Website
 
 exit $?
